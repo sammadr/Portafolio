bb0aff72f6282b5ab30814f9ccb45eeed9325c42
Samantha Diaz <20212025@itla.edu.do>
2024-07-16 19:46:29 -0400

    agregando archivo filtro.js para poderl filtrar los proyectos de la seccion portalio

---
+++ js/filtro.js
@@ -0,0 +1,27 @@
+const filterButtons = document.querySelectorAll('.botonFiltro');
+const cards = document.querySelectorAll('.cards');
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const category = this.getAttribute('data-categoria');
+
+    // Quitar la clase 'active' de todos los botones
+    filterButtons.forEach(btn => btn.classList.remove('active'));
+
+    // Agregar la clase 'active' al botón clicado
+    this.classList.add('active');
+
+    // Mostrar/Ocultar tarjetas
+    cards.forEach(card => {
+      if (category === 'all') {
+        card.style.display = 'block';
+      } else {
+        if (card.id === category) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      }
+    });
+  });
+});
