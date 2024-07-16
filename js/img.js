ac42b466e5bde707c9189b710018eb4f9b44a7a3
Samantha Diaz <20212025@itla.edu.do>
2024-07-15 22:45:14 -0400

    Agregando img.js donde podemos ver la logica para el cambio de imagen en la seccion sobre

---
+++ js/img.js
@@ -0,0 +1,19 @@
+const html = document.querySelector('html');
+const botonImg = document.querySelector('.boton-img');
+const img = document.querySelector('.img-boton');
+const titulo = document.querySelector('.sobre-text');
+
+let cambioImg = true;
+
+botonImg.addEventListener('click', () => {
+  if (cambioImg) {
+    img.setAttribute('src', './assets/samma-icon.png');
+    titulo.innerHTML = "Ya me viste, haz clic"
+  } else {
+    img.setAttribute('src', './assets/samma-ia.jpg');
+    titulo.innerHTML = "Haz clic para verme"
+  }
+  cambioImg = !cambioImg; // Alternar el estado
+  
+});
+
